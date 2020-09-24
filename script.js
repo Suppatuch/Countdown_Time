2d691b5c4c8f30f2b62a06fa193afda1e3408259
Suppatuch <41631450+Suppatuch@users.noreply.github.com>
2020-09-24 12:48:59 +0700

    Add initial Countdown website

---
+++ script.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = '1 Jan 2021';
+
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+    const totleseconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(totleseconds / 3600 / 24);
+    const hours = Math.floor(totleseconds / 3600) % 24;
+    const minutes = Math.floor(totleseconds / 60) % 60;
+    const seconds = Math.floor(totleseconds) % 60;
+    
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+    //console.log(days,hours,minutes,seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+
+countdown();
+
+setInterval(countdown, 1000);
